0d5eded0a6265e67d28936f0d8c1fcc6a53d64a2
KK <159507050+Kkboi2000@users.noreply.github.com>
2026-07-03 02:01:02 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -7,6 +7,6 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 const SUPABASE_URL = 'https://euieccuakerbgbheljqs.supabase.co';
 const SUPABASE_ANON_KEY =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5qd2NhaHRlaHVudnJqdWxnemp4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQxODE5NDIsImV4cCI6MjA4OTc1Nzk0Mn0.zhXXVu43AY4DTjlLQC3TDZVdiLQ44tg1e4OBdfVhe-o';
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV1aWVjY3Vha2VyYmdiaGVsanFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMwMDA0NDQsImV4cCI6MjA5ODU3NjQ0NH0.xquURJT39TPE2O2B7DT1H4lRPICxa38W7y3XtrHKQPc';
 
 export const sb = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
