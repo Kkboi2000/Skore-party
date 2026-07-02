9493bcf98b6ceea8fe4d05560d38e8fc12bc5fd7
KK <159507050+Kkboi2000@users.noreply.github.com>
2026-07-03 01:50:17 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,8 +5,8 @@
    ============================================================ */
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
-const SUPABASE_URL = 'https://esplnvjwdvlnhdtnmdiz.supabase.co';
+const SUPABASE_URL = 'https://euieccuakerbgbheljqs.supabase.co';
 const SUPABASE_ANON_KEY =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVzcGxudmp3ZHZsbmhkdG5tZGl6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI3MzI4MTAsImV4cCI6MjA5ODMwODgxMH0.H0bdShHeL59p8ql0LAuL8991VGGFtXyYbCl1GjWSC8g';
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5qd2NhaHRlaHVudnJqdWxnemp4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQxODE5NDIsImV4cCI6MjA4OTc1Nzk0Mn0.zhXXVu43AY4DTjlLQC3TDZVdiLQ44tg1e4OBdfVhe-o';
 
 export const sb = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
